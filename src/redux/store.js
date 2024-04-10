705ed06c8e4861a9886481a700b33efba9c28242
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-10 23:53:57 +0200

    bug fixed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,8 @@
-import { contactsReducer } from "./contactsSlice";
 import { configureStore } from "@reduxjs/toolkit";
-import { filtersReducer } from "./filtersSlice";
-import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
 import storage from "redux-persist/lib/storage"
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import { contactsReducer } from "./contacts/slice";
+import { filtersReducer } from "./filters/slice";
 import { authReducer } from "./auth/slice";
 
 const authConfig = {
--- src/selectors/selectors.js
+++
@@ -1,7 +0,0 @@
-export const selectIsLoading = state => state.contacts.isLoading;
-
-export const selectIsError = state => state.contacts.error;
-
-export const selectContacts = state => state.contacts.items;
-
-// export const selectNameFilter = state => state.filters.name;
